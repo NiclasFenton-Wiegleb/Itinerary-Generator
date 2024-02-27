6ddf8e4220b65fb5007414642a96f6f2cdb074f2
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-02-27 21:40:38 +0000

    Organising App layout.

--- app.py
+++ app.py
@@ -35,13 +35,13 @@ def clicked(button):
     st.session_state.clicked[button] = True
 
 if 'route_idx' not in st.session_state:
-    st.session_state.route_idx = 0
+    st.session_state.route_idx = [1:0]
 
 st.write(st.session_state.route_idx)
 
 @st.cache_data
 def select_route(button):
-    st.session_state.route_idx = random.randint(0, len(route_data))
+    st.session_state.route_idx[button] = random.randint(0, len(route_data))
 
 # Button with callback function
 button = st.button("Generate Itinerary", on_click=select_route, args=[1])
