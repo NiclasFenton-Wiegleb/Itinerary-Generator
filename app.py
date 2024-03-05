94ec1ea20b730134c1bd82411bb4b79fa4dbedaf
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-05 21:47:30 +0000

    Working on route index to stay in cache data.

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if st.session_state.clicked[1] == True:
 
         column = str(alt_lst[n])
         st.write(route_data.columns)
-        stop_1 = int(route_data[column][st.session_state.route_idx])
+        stop_1 = int(dataset[column][stop_1])
 
         col1.write(st.session_state.clicked[1])
 
