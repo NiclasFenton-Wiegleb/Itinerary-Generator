049b9600f8a8a44f8610e4dbb09f44c126181aba
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-19 19:31:03 +0000

    Updating button symbols.

--- app.py
+++ app.py
@@ -181,11 +181,11 @@ if st.session_state.button[1] == True:
         y = x+10
 
          # Next Button
-        next_txt = "➵"
+        next_txt = "⇀"
         next_stop = col3.button(next_txt, on_click=next_func, key=x, args=[state])
 
         # Previous Button
-        prev_txt = "Previous"
+        prev_txt = "↼"
         prev_stop = col1.button(prev_txt, on_click=prev_func, key=y, args=[state])
 
         if state == 3:
