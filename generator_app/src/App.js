f5910744858b8d55ab2f27b44125cb8a02f7c17d
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 17:46:23 +0000

    Adding AdSense componentns.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -5,27 +5,41 @@ import './App.css';
 function App() {
   return (
   <div className="App">
-    {/* <div className="left-component">
+    <div className="left-component">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
      crossorigin="anonymous">
      </script>
      {/* <!-- left component --> */}
-     {/* <ins class="adsbygoogle"
+     <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-6270659904604748"
      data-ad-slot="4698668960"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
      <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});</script> */}
-    {/* </div> */}
+     (adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
+    <div className="right-component">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
+      crossorigin="anonymous"></script>
+      {/* <!-- right component --> */}
+      <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-6270659904604748"
+      data-ad-slot="4526444425"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
     <div className="iFrame">
     <iframe
       allowfullscreen
       id="myiframe"
       src="https://itinerary-generator-hg7qp7yihprqdbjzvylk5u.streamlit.app/?embed=true&?embed_options=light_theme"
-      height="800"
-      width="1000"
+      // height="800"
+      // width="1000"
       style={{border: "none" }}
       title="Itinerary Generator"></iframe>
     </div>
