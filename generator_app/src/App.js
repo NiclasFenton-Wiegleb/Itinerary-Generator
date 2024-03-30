499ffc905d5500083fb110dab4a96c25c09462da
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 11:40:11 +0000

    Troubleshooting AdSense integration.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import AdsenseComponent from './AdSenseComponent';
         
 
 function App() {
   return (
   <div className="App">
-    <head>
+    <div className="left-component">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
-     crossorigin="anonymous"></script>
-    </head>
+     crossorigin="anonymous">
+     </script>
+     {/* <!-- left component --> */}
+     <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6270659904604748"
+     data-ad-slot="4698668960"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+     <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
+    <div className="iFrame">
     <iframe
       allowfullscreen
       id="myiframe"
@@ -16,7 +28,8 @@ function App() {
       width="1000"
       style={{border: "none" }}
       title="Itinerary Generator"></iframe>
-      </div>
+    </div>
+  </div>
   );
 }
 
