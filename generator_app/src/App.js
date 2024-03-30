80a3fd69a806a9dfa94b4234cd7099a25389f9cc
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 11:00:53 +0000

    Injecting Google AdSense code.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -1,8 +1,57 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+ } from "react-router-dom";
+
+ import React, { useEffect } from 'react';
+
+// const AdsenseComponent = ({ adClient, adSlot, adFormat }) => {
+//   useEffect(() => {
+//     (window.adsbygoogle = window.adsbygoogle || []).push({});
+//   }, []);
+
+//   return (
+//     <ins className="adsbygoogle"
+//          style={{ display: 'block' }}
+//          data-ad-client={adClient}
+//          data-ad-slot={adSlot}
+//          data-ad-format={adFormat}></ins>
+//   );
+// };
+
+// export default AdsenseComponent;
+
+function NotFoundPage() {
+  return (
+    <div>
+      Page not found
+    </div>
+  );
+ }
+ 
+        
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <body>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
+        crossorigin="anonymous">
+        </script>
+        {/* <!-- left side bar ad --> */}
+        <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6270659904604748"
+        data-ad-slot="4526444425"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </body>
       <iframe
       allowfullscreen
       id="myiframe"
@@ -11,11 +60,8 @@ function App() {
       width="1000"
       style={{border: "none" }}
       title="Itinerary Generator"></iframe>
-      <script>
-        var iframe = document.getElementById("myiframe");
-        iframe.width = iframe.contentWindow.document.body.scrollWidth;
-        iframe.height = iframe.contentWindow.document.body.scrollHeight;
-      </script>
+      <Routes><Route path="/*" element={<NotFoundPage />}/></Routes>
+      </BrowserRouter>
     </div>
   );
 }
