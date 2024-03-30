e4ef7e8653e5e7f6480d43526298223e0e3de21a
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 18:01:30 +0000

    Adding AdSense componentns.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -6,22 +6,25 @@ function App() {
   return (
   <div className="App">
     <div className="left-component">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
-     crossorigin="anonymous">
-     </script>
-     {/* <!-- left component --> */}
-     <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6270659904604748"
-     data-ad-slot="4698668960"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-     <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
+      crossorigin="anonymous">
+
+      </script>
+      {/* <!-- left component --> */}
+      <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-6270659904604748"
+      data-ad-slot="4698668960"
+      data-ad-format="auto"
+      data-full-width-responsive="true">
+    
+      </ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
     <div className="right-component">
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
-      crossorigin="anonymous"></script>
+      crossorigin="anonymous">
+      </script>
       {/* <!-- right component --> */}
       <ins class="adsbygoogle"
       style="display:block"
@@ -29,19 +32,19 @@ function App() {
       data-ad-slot="4526444425"
       data-ad-format="auto"
       data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
     <div className="iFrame">
-    <iframe
+      <iframe
       allowfullscreen
       id="myiframe"
       src="https://itinerary-generator-hg7qp7yihprqdbjzvylk5u.streamlit.app/?embed=true&?embed_options=light_theme"
       // height="800"
       // width="1000"
       style={{border: "none" }}
-      title="Itinerary Generator"></iframe>
+      title="Itinerary Generator">
+        
+      </iframe>
     </div>
   </div>
   );
