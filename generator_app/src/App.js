0ce8166b1896760116312ad3ecbca0e2ef31ba69
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 11:12:00 +0000

    Troubleshooting AdSense integration.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -38,19 +38,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <body>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6270659904604748"
-        crossorigin="anonymous">
-        </script>
-        {/* <!-- left side bar ad --> */}
-        <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-6270659904604748"
-        data-ad-slot="4526444425"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
       </body>
       <iframe
       allowfullscreen
