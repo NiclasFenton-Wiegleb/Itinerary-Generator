e1d65868e8f3b25028b1440c864c149a19941172
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-03-30 11:14:40 +0000

    Troubleshooting AdSense integration.

--- generator_app/src/App.js
+++ generator_app/src/App.js
@@ -5,24 +5,6 @@ import {
   Routes,
  } from "react-router-dom";
 
- import React, { useEffect } from 'react';
-
-// const AdsenseComponent = ({ adClient, adSlot, adFormat }) => {
-//   useEffect(() => {
-//     (window.adsbygoogle = window.adsbygoogle || []).push({});
-//   }, []);
-
-//   return (
-//     <ins className="adsbygoogle"
-//          style={{ display: 'block' }}
-//          data-ad-client={adClient}
-//          data-ad-slot={adSlot}
-//          data-ad-format={adFormat}></ins>
-//   );
-// };
-
-// export default AdsenseComponent;
-
 function NotFoundPage() {
   return (
     <div>
