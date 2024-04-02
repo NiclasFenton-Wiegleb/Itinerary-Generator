bf0def876a59f5f06e417983df55cc90d22d6b58
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-02 21:41:52 +0100

    Making App responsive to desktop and mobile view.

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ if st.session_state.button[1] == True:
     
     for x, item in enumerate(stop_lst):
 
-        col1, col2, col3 = st.columns((1, 2,1))
+        col1, col2, col3 = st.columns([1, 1,1])
 
         state = state_lst[x]
         next_func = next_lst[x]
