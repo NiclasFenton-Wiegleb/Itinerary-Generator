7235f69193fc64188cb9815f455e29fd812a2b75
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-27 10:05:53 +0100

    Fixing issue of buttons going over rows on mobile view.

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ if st.session_state.button[1] == True:
 
                 st.markdown(f"""## {title_lst[x]}""")  # title
                 st.markdown(f"""### {dataset.name.iloc[stop]}""")  # name
-                st.image([image_1, image_2], caption= ["", f"""Source: br><a href={dataset.img_source.iloc[stop]} target="_blank">Link to website</a>"""], width = 300) # images
+                st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], width = 300) # images
                 with st.container():
                     st.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
                     st.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
@@ -294,7 +294,7 @@ if st.session_state.button[1] == True:
                 with col2:
                     st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], use_column_width= True, width = 100) # images
                 col2.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
-                col2.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
+                col2.write(f"""<br><a href={dataset.link.iloc[stop]}target="_blank">Link to website</a""") #link to website
 
                 long_lst[x] = dataset.long_coordinates.iloc[stop] #longitude
                 lat_lst[x] = dataset.lat_coordinates.iloc[stop] #latitude
