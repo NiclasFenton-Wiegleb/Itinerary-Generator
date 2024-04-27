ff5c314202a82e8ef62773064aeaa915b2226032
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-27 09:56:35 +0100

    Fixing issue of buttons going over rows on mobile view.

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def fix_mobile_columns():
 #Fixin scroll issue on mobil version
 def fix_horizontal_scroll():
     st.write('''<style>
-    html, body {
+    section.main > div: {
     max-width: 100%;
     overflow-x: hidden;
     }
