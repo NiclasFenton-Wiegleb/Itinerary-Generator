e46b74e08556ff037b78ccd89db0561feef67cfe
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-28 12:29:38 +0100

    Fixing issue of buttons going over rows on mobile view.

--- app.py
+++ app.py
@@ -276,14 +276,9 @@ if st.session_state.button[1] == True:
                 image_2 = retrieve_img(f"""./images/{dataset.name.iloc[stop]}_002.jpg""")
 
                 with st.container():
-                    fix_vertical_scroll()
                     st.markdown(f"""## {title_lst[x]}""")  # title
                     st.markdown(f"""### {dataset.name.iloc[stop]}""")  # name
-                
-                st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], width = 300) # images
-
-                with st.container():
-                    fix_vertical_scroll()
+                    st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""], width = 300) # images
                     st.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
                     st.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
 
