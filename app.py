c01ca0bf4d4ee268cae9dff52c1f7090171ee505
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-04-28 13:15:06 +0100

    Optimising desktop version of App.

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ if st.session_state.button[1] == True:
 
             if state == 3:
 
-                # col4, col5, col6 = st.columns((1,2,1), gap="small")
+                col4, col5, col6 = st.columns((1,2,1), gap="small")
 
                 #Id stop for Brunch
                 stop = int(route_data[item][st.session_state.route_idx])
@@ -275,13 +275,12 @@ if st.session_state.button[1] == True:
                 image_1 = retrieve_img(f"""./images/{dataset.name.iloc[stop]}_001.jpg""")
                 image_2 = retrieve_img(f"""./images/{dataset.name.iloc[stop]}_002.jpg""")
 
-                with st.container():
-                    fix_vertical_scroll()
-                    st.markdown(f"""## {title_lst[x]}""")  # title
-                    st.markdown(f"""### {dataset.name.iloc[stop]}""")  # name
-                    st.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""]) # images
-                    st.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
-                    st.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
+
+                col5.markdown(f"""## {title_lst[x]}""")  # title
+                col5.markdown(f"""### {dataset.name.iloc[stop]}""")  # name
+                col5.image([image_1, image_2], caption= ["", f"""Source: {dataset.img_source.iloc[stop]}"""]) # images
+                col5.write(f"""Address: {dataset.address.iloc[stop]}""")  # address
+                col5.write(f"""Link: {dataset.link.iloc[stop]}""") #link to website
 
                 long_lst[x] = dataset.long_coordinates.iloc[stop] #longitude
                 lat_lst[x] = dataset.lat_coordinates.iloc[stop] #latitude
