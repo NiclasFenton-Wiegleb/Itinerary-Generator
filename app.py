0776d7c38db838e328723994a50d46309a6f91cb
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2024-05-02 18:46:54 +0100

    Tidying up backend and polishing look.

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ def app_layout():
                .css-1d391kg {
                     padding-top: 3.5rem;
                     padding-right: 1rem;
-                    padding-bottom: 3.5rem;
+                    padding-bottom: 10rem;
                     padding-left: 1rem;
                 }
         </style>
